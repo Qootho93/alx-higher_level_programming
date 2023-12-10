7419e160beb210126192e7cfcfa43318bd97a51e
Qootho93 <preciousqootho@gmail.com>
2023-12-10 13:06:38 -0800

    102-complex_delete.py

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    for key in list(a_dictionary):
+        if a_dictionar[key] == value:
+            del s_dictionary[key]
+    return a_dictionary
