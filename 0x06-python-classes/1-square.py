8358f380eeff2bcf94f95b3b85ba0d6435211781
Qootho93 <preciousqootho@gmail>
2023-12-19 12:30:54 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+class Square:
+    """2Represent a square."""
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+        size (int):
+        The size of new square.
+        """
+        self.__size = size
