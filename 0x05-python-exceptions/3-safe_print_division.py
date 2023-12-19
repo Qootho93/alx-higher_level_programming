b5ae14bf717d1393cab49cec5da3c4f8e07b7e21
Qootho93 <preciousqootho@gmail>
2023-12-18 16:54:33 -0800

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         answer = a / b
-    except (ZeroDivisionError, FloatingPointError):
+    except (ZeroDivisionError):
         answer = None
     finally:
         print("Insight result: {}".format(answer))
